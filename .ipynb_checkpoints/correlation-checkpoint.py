5dd30b0f57e9d5277afb81fec935c1609c120637
unknown <fidahani25@gmail.com>
2019-07-26 00:37:01 +0700

    correlation measurement

---
+++ .ipynb_checkpoints/correlation-checkpoint.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
+from scipy.spatial.distance import cosine
